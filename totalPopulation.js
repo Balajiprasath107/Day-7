08e16bd90f5c3f59bd5cfd4ba815ccedc6cca943
Balajiprasath T <82133700+Balajiprasath107@users.noreply.github.com>
2024-06-09 09:28:33 +0530

    Create totalPopulation.js

---
+++ totalPopulation.js
@@ -0,0 +1,13 @@
+var link = "https://restcountries.com/v3.1/all"
+var xml = new XMLHttpRequest()
+xml.open("GET",link,true)
+xml.send()
+xml.onreadystatechange=function(){
+  if(this.readyState===4 && this.status===200){
+var data = (JSON.parse(this.response)) 
+ console.log(data.reduce((acc,curr)=> (acc+curr.population),0))
+  }
+}
+
+/* output
+  7777721563
